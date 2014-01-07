5c92d2665cbf1ba38cc59333a3d7f77451be37fa
Shin Kira <shinkira3@gmail.com>
2014-01-07 16:52:36 -0500

    r (observable firing rate) saved for each trial
    This code is slow, and needs a speed up.

--- frWoe2dDynSimCalc.m
+++ frWoe2dDynSimCalc.m
@@ -1,4 +1,4 @@
-function out = frWoe2dSimCalc(theta,info,varargin)
+function out = frWoe2dDynSimCalc(theta,info,varargin)
 
 % This test code compares bounded accumulation between
 % Monte-Carlo simulation and Fokker-Plank propagation.
@@ -23,6 +23,7 @@ fig_switch = 0;
 varargin2V(varargin);
 symmetric_flag = 0; % if 1, x1_dfr(ei) = x2_dfr(-ei); if 0, if 1, x1_dfr(ei) = -x2_dfr(ei)
 
+num_trial = 5e3;
 num_step = 10;
 % Best fitted parameters for singly stochastic model.
 % num_param: 
@@ -56,6 +57,22 @@ deci_WOE = -9:2:9;
 shape_prob = make790StimProb(1);
 cum_shape_prob = cumsum(shape_prob);
 cum_shape_prob_flip = cumsum(fliplr(shape_prob));
+
+randX = rand(num_trial,num_step);
+shape_woe = nan(size(randX));
+shape_ind = nan(size(randX));
+for i = 1:length(shape_prob);
+    shape_woe(randX>0 & randX<=shape_prob(i)) = deci_WOE(i);
+    shape_ind(randX>0 & randX<=shape_prob(i)) = i;
+    randX = randX - shape_prob(i);
+end
+
+if 1
+    % Shapes are sampled from the other distribution for the latter half of trials.
+    shape_woe(num_trial/2+1:end,:) = -shape_woe(num_trial/2+1:end,:);
+    shape_ind(num_trial/2+1:end,:) = 11-shape_ind(num_trial/2+1:end,:);
+end
+
 
 switch info.id
     case 1
@@ -131,7 +148,7 @@ end
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % preallocation
-num_trial = 1e4;
+
 x1_fr = nan(num_trial,num_step+1);
 x2_fr = nan(num_trial,num_step+1);
 
@@ -142,10 +159,15 @@ x1_error_fr = nan(num_trial,1);
 x2_error_fr = nan(num_trial,1);
 result = nan(num_trial,1);
 choice = nan(num_trial,1);
+GoRT = nan(num_trial,1);
+rt = nan(num_trial,1);
 num_accum = nan(num_trial,1);
 cum_woe_correct = nan(num_trial,1);
 cum_woe_error = nan(num_trial,1);
 cum_woe_end = nan(num_trial,1);
+
+r1 = nan(num_trial,length(t));
+r2 = nan(num_trial,length(t));
 
 %%
 urgency = cumsum([0,mean_epoch_dfr_v]);
@@ -157,12 +179,9 @@ end
 
 shift_num = tnd_pre/10;
 urgency_dt = circshift([urgency_dt,zeros(1,100)],[0,shift_num]);
-urgency_dt = urgency_dt(1:250);
+urgency_dt = urgency_dt(1:251);
 
 for k = 1:num_trial
-    
-    
-    
     if mod(k,1e3)==0
         printf('%d\n',k)
     end
@@ -178,11 +197,13 @@ for k = 1:num_trial
     hr2 = nan(1,length(t));
     hr1(1) = x1_fr(k,1);
     hr2(1) = x2_fr(k,1);
+    r1(k,1) = hr1(1);
+    r2(k,1) = hr2(1);
     
     % observable rate (urgency added)
 
-    r1 = nan(1,length(t));
-    r2 = nan(1,length(t));
+    % r1 = nan(1,length(t));
+    % r2 = nan(1,length(t));
 
     cum_woe(k,1) = 0;
     N = 0;
@@ -191,25 +212,8 @@ for k = 1:num_trial
     for ti = 1:length(t)-1
         
         if mod((t(ti) - tnd_pre),250)==0
-            N = N+1;
-        end
-        
-        if mod((t(ti) - tnd_pre),250)==0
-            
+            tic
             ei = ei+1;
-            
-            % A new shape affects the firing rate 200ms after the onset.        
-
-                % WOE
-            if k<=num_trial/2   
-                shape_ind = find(rand(1)<=cum_shape_prob,1,'first');
-            else
-                shape_ind = find(rand(1)<=cum_shape_prob_flip,1,'first');
-            end
-
-            shape_woe = deci_WOE(shape_ind);
-            cum_woe(k,ei+1) = cum_woe(k,ei) + shape_woe;
-
                 % FR
             noise1 = dfr_sd * randn(1);
             switch noise_type
@@ -218,19 +222,17 @@ for k = 1:num_trial
                 case 'ind'
                     noise2 = dfr_sd * randn(1);
             end
-
+            % toc
             pos_ind = heaviside(shape_woe);
-
-            sig1 = dfr_v(shape_ind)  + noise1;
+            sig1 = dfr_v(shape_ind(k,ei))  + noise1;
             if symmetric_flag
-                sig2 = dfr_v(11-shape_ind) + noise2;
+                sig2 = dfr_v(11-shape_ind(k,ei)) + noise2;
             else
-                sig2 = -dfr_v(shape_ind) + noise2;
+                sig2 = -dfr_v(shape_ind(k,ei)) + noise2;
             end
-
             x1_fr(k,ei+1) = x1_fr(k,ei) + sig1;
             x2_fr(k,ei+1) = x2_fr(k,ei) + sig2;
-
+            % toc
             % Assuming the pupulation FR cannot go below min_fr
             if 1
                 if x1_fr(k,ei+1)<min_fr
@@ -241,66 +243,71 @@ for k = 1:num_trial
                 end
             end
         end
-        
+        % toc
         hr1(ti+1) = hr1(ti) + dt/tau_r*(x1_fr(k,ei+1) - hr1(ti));
         hr2(ti+1) = hr2(ti) + dt/tau_r*(x2_fr(k,ei+1) - hr2(ti));
         
-        if ei>0
-           r1(ti+1) = hr1(ti+1) + urgency_dt(ti);
-           r2(ti+1) = hr2(ti+1) + urgency_dt(ti);
-        end
-        
-        
+        r1(k,ti+1) = hr1(ti+1) + urgency_dt(ti);
+        r2(k,ti+1) = hr2(ti+1) + urgency_dt(ti);
+        % toc
         % if FR of either neuron reaches the bound...
-        if (r1(ti+1)>B) || (r2(ti+1)>B)
-            
-            rt(k) = t(ti+1) + tnd_post;
-            num_accum(k) = ei;
-            cum_woe_end(k) = cum_woe(k,ei+1);
+        if (r1(k,ti+1)>B) || (r2(k,ti+1)>B)
+            rt(k,1) = t(ti+1) + tnd_post;
 
-            if (r1(ti+1)>B) > (r2(ti+1)>B)
+            if rt(k,1)>=2500
+                continue
+            end
+            
+            N = floor(rt(k,1)/250)+1;
+            GoRT(k,1) = mod(rt(k,1),250);
+            num_accum(k) = ei;
+            
+            cum_woe(k,1:N) = cumsum(shape_woe(k,1:N));
+            cum_woe_end(k) = cum_woe(k,ei);
+
+            if (r1(k,ti+1)>B) > (r2(k,ti+1)>B)
                 % simulate a saccadic burst
 
                 choice(k) = 1;
                 
                 if k <= num_trial/2
-                    r1_correct_fr(k) = r1(ti+1);
-                    r2_correct_fr(k) = r2(ti+1);
+                    r1_correct_fr(k) = r1(k,ti+1);
+                    r2_correct_fr(k) = r2(k,ti+1);
                     cum_woe_correct(k) = cum_woe(k,ei+1);
                     result(k) = 1; % correct
                 else
-                    r1_error_fr(k) = r1(ti+1);
-                    r2_error_fr(k) = r2(ti+1);
+                    r1_error_fr(k) = r1(k,ti+1);
+                    r2_error_fr(k) = r2(k,ti+1);
                     cum_woe_error(k) = cum_woe(k,ei+1);
                     result(k) = 0; % error
                 end
 
-            elseif (r1(ti+1)>B) < (r2(ti+1)>B)
+            elseif (r1(k,ti+1)>B) < (r2(k,ti+1)>B)
                 % simulate a saccadic burst
                 
                 choice(k) = 2;
                 
                 if k <= num_trial/2
-                    r1_error_fr(k) = r1(ti+1);
-                    r2_error_fr(k) = r2(ti+1);
+                    r1_error_fr(k) = r1(k,ti+1);
+                    r2_error_fr(k) = r2(k,ti+1);
                     cum_woe_error(k) = cum_woe(k,ei+1);
                     result(k) = 0; % error
                 else
-                    r1_correct_fr(k) = r1(ti+1);
-                    r2_correct_fr(k) = r2(ti+1);
+                    r1_correct_fr(k) = r1(k,ti+1);
+                    r2_correct_fr(k) = r2(k,ti+1);
                     cum_woe_correct(k) = cum_woe(k,ei+1);
                     result(k) = 1; % correct
                 end
 
             else % Tied case: this heppens very rarely, if any.
                 if rand(1)>0.5
-                    r1_correct_fr(k) = r1(ti+1);
-                    r2_correct_fr(k) = r2(ti+1);
+                    r1_correct_fr(k) = r1(k,ti+1);
+                    r2_correct_fr(k) = r2(k,ti+1);
                     cum_woe_correct(k) = cum_woe(k,ei+1);
                     result(k) = 1; % correct
                 else
-                    r1_error_fr(k) = r1(ti+1);
-                    r2_error_fr(k) = r2(ti+1);
+                    r1_error_fr(k) = r1(k,ti+1);
+                    r2_error_fr(k) = r2(k,ti+1);
                     cum_woe_error(k) = cum_woe(k,ei+1);
                     result(k) = 0; % error
                 end
@@ -313,106 +320,14 @@ for k = 1:num_trial
             end
             
             if 0
-                if r1(ti+1)>B
-                    r1(ti+1) = B;
+                if r1(k,ti+1)>B
+                    r1(k,ti+1) = B;
                 end
-                if r2(ti+1)>B
-                    r2(ti+1) = B;
+                if r2(k,ti+1)>B
+                    r2(k,ti+1) = B;
                 end
             end
             break
-        end
-        
-        
-        if 0
-            % if FR of either neuron reaches the bound...
-            if (x1_fr(k,ei+1)>B) || (x2_fr(k,ei+1)>B)
-                num_accum(k) = ei;
-                cum_woe_end(k) = cum_woe(k,ei+1);
-                
-                if x1_fr(k,ei+1)>x2_fr(k,ei+1)
-                    % simulate a saccadic burst
-                    x1_fr(k,ei+1) = x1_fr(k,ei+1) + burst;
-                    % x1_fr(k,ei+1) = B;
-                    choice(k) = 1;
-
-                    if symmetric_flag
-                        x1_correct_fr(k) = x1_fr(k,ei+1);
-                        x2_correct_fr(k) = x2_fr(k,ei+1);
-                        cum_woe_correct(k) = cum_woe(k,ei+1);
-                        result(k) = 1;
-                    else
-                        if k <= num_trial/2
-                            x1_correct_fr(k) = x1_fr(k,ei+1);
-                            x2_correct_fr(k) = x2_fr(k,ei+1);
-                            cum_woe_correct(k) = cum_woe(k,ei+1);
-                            result(k) = 1;
-                        else
-                            x1_error_fr(k) = x1_fr(k,ei+1);
-                            x2_error_fr(k) = x2_fr(k,ei+1);
-                            cum_woe_error(k) = cum_woe(k,ei+1);
-                            result(k) = 0;
-                        end
-                    end
-
-                elseif x1_fr(k,ei+1)<x2_fr(k,ei+1)
-                    % simulate a saccadic burst
-                    x2_fr(k,ei+1) = x2_fr(k,ei+1) + burst;
-                    % x2_fr(k,ei+1) = B;
-                    choice(k) = 2;
-
-                    if symmetric_flag
-                        x1_error_fr(k) = x1_fr(k,ei+1);
-                        x2_error_fr(k) = x2_fr(k,ei+1);
-                        cum_woe_error(k) = cum_woe(k,ei+1);
-                        result(k) = 0;
-                    else
-                        if k <= num_trial/2
-                            x1_error_fr(k) = x1_fr(k,ei+1);
-                            x2_error_fr(k) = x2_fr(k,ei+1);
-                            cum_woe_error(k) = cum_woe(k,ei+1);
-                            result(k) = 0;
-                        else
-                            x1_correct_fr(k) = x1_fr(k,ei+1);
-                            x2_correct_fr(k) = x2_fr(k,ei+1);
-                            cum_woe_correct(k) = cum_woe(k,ei+1);
-                            result(k) = 1;
-                        end
-                    end
-
-                else % Tied case: this heppens very rarely, if any.
-                    if rand(1)>0.5
-                        x1_correct_fr(k) = x1_fr(k,ei+1);
-                        x2_correct_fr(k) = x2_fr(k,ei+1);
-                        cum_woe_correct(k) = cum_woe(k,ei+1);
-                        % x1_fr(k,ei+1) = nan;
-                        % x2_fr(k,ei+1) = nan;
-                        result(k) = 1;
-                    else
-                        x1_correct_fr(k) = x1_fr(k,ei+1);
-                        x2_correct_fr(k) = x2_fr(k,ei+1);
-                        cum_woe_error(k) = cum_woe(k,ei+1);
-                        % x1_fr(k,ei+1) = nan;
-                        % x2_fr(k,ei+1) = nan;
-                        result(k) = 0;
-                    end
-
-                    if rand(1)>0.5
-                        choice(k) = 1;
-                    else
-                        choice(k) = 2;
-                    end
-                end
-                if 0
-                    if x1_fr(k,ei+1)>B
-                        x1_fr(k,ei+1) = B;
-                    end
-                    if x2_fr(k,ei+1)>B
-                        x2_fr(k,ei+1) = B;
-                    end
-                end
-                break
-            end
         end
     end
 end
@@ -461,7 +376,7 @@ end
 out.betaFit = betaFit;
 
 %% RT distribution
-    figure(fig+10);clf;hold on;
+    figure(fig+1);clf;hold on;
     set(gcf,'position',[0,0,1000,400],'color','w')
     rt_obs_hist = histc(info.RT,0:3000);
     rt_pred_hist = histc(rt,0:3000);
@@ -473,8 +388,6 @@ out.betaFit = betaFit;
     p_rt_pred_hist = rt_pred_hist./sum(rt_pred_hist);
     p_rt_pred_hist(1) = 0; % force to zero
 
-    % fill(0:3000,rt_obs_hist,'k');
-    % fill(0:3000,rt_pred_hist,'r');
     subplot(2,1,1)
     % fill(0:3000,p_rt_obs_hist,'k');
     fill(t,p_rt_obs_hist_10ms','k-')
@@ -488,4 +401,81 @@ out.betaFit = betaFit;
     plot(0:3000,cumsum(p_rt_obs_hist),'k-')
     plot(0:3000,cumsum(p_rt_pred_hist),'r-')
     
+%%  
+    % load sim0107.mat
+    LLR{1} = diff(cum_woe,1,2);
+    LLR{2} = nan(size(LLR{1}));
+    for ei = 1:10
+        pick = isfinite(LLR{1}(:,ei)); 
+        num_shift = 10-ei;
+        LLR{2}(pick,:) = fliplr(circshift(LLR{1}(pick,:),[0,num_shift]));
+    end
+
+    % *********** Backward Analysis ***********
+    lastStim2Sac = GoRT;
+    LLR_back = LLR{2}/10;
+    shift_size = 10;
+    bin_size = 40;
+    num_bin = 60;
+    trial_id = (1:num_trial)';
+    
+    for ti = 1:num_bin
+        
+        pick_before = logical(lastStim2Sac>=0);
+        pick_after = logical(lastStim2Sac-bin_size>=0);
+        pickDiff = logical((pick_before-pick_after)==1);
+        
+        % remove shapes for the next iteration
+        lastStim2Sac = lastStim2Sac - shift_size;
+        pick_res = logical(lastStim2Sac - shift_size>=0);
+        pick_remove = logical((pick_before-pick_res)==1);
+        lastStim2Sac(pick_remove) = lastStim2Sac(pick_remove) + 250;
+        % LLR_picked = LLR_back(pickDiff,1);
+        for ri = 1;
+            
+            pick = logical(pickDiff & isfinite(LLR_back(:,1)) & trial_id<=num_trial/2);
+            
+            LLR_picked = LLR_back(pick,1);
+            resLLR_picked = nansum(LLR_back(pick,2:end),2); % sum of logLR for the rest of shapes
+            choice_picked = choice(pick);
+            [logitCoef,dev,stats] = glmfit([LLR_picked, resLLR_picked],2-choice_picked,'binomial','link','logit');
+            % Convert the log base from exp to 10
+            logitCoef = logitCoef/log(10);
+            stats.se = stats.se/log(10);
+            
+            glm_w(ri,ti,:) = logitCoef;
+            glm_w_ci(ri,ti,:) = stats.se * norminv(0.975,0,1);
+            glm_w_lo(ri,ti,:) = logitCoef - stats.se * norminv(0.975,0,1);
+            glm_w_hi(ri,ti,:) = logitCoef + stats.se * norminv(0.975,0,1);
+            glm_p(ri,ti,:) = stats.p;
+            
+        end
+        
+        % shift the matrix for trials that were picked and replace with NaN.
+        LLR_back(pick_remove,:) = circshift(LLR_back(pick_remove,:),[0 -1]);
+        LLR_back(pick_remove,end) = nan;
+        
+    end
+    figure(fig);shg
+    subplot(1,2,2);hold on;
+    t_bin_center = bin_size/2 + shift_size.*((1:num_bin)-1);
+    
+    if 0
+        h1 = ploterr(-t_bin_center,squeeze(glm_w(1,:,2)),[],{squeeze(glm_w_lo(1,:,2)),squeeze(glm_w_hi(1,:,2))},1,'ro-','abshhy',0);
+        h2 = ploterr(-t_bin_center,squeeze(glm_w(2,:,2)),[],{squeeze(glm_w_lo(2,:,2)),squeeze(glm_w_hi(2,:,2))},1,'bo-','abshhy',0);
+        set(h1(1),'MarkerFaceColor','r');
+        set(h2(1),'MarkerFaceColor','b');
+    else
+        fillTrace(-t_bin_center,squeeze(glm_w(1,:,2)),squeeze(glm_w_ci(1,:,2)),0.2*[1,1,1]);
+        % h3 = ploterr(-t_bin_center,squeeze(glm_w(1,:,2)),[],{squeeze(glm_w_lo(1,:,2)),squeeze(glm_w_hi(1,:,2))},1,'ko-','abshhy',0);
+        % set(h3(1),'MarkerFaceColor','k');
+    end
+    hold on
+    plot(-t_bin_center,0*ones(1,num_bin),'k--');
+
+    xlim([-600 0]);
+    ylim([-0.5 3])
+    set(gcf,'position',[500 100 400 300],'color','w');
+    set(gca,'FontSize',24,'FontWeight','bold','Box','off','TickDir','out','YTick',[],'YAxisLocation','right')
+
 end
